fce4294127f9c8f8fef1e0893dd47ddf1f8bf694
Joseph Reed <chicobitcoinjoe@gmail.com>
2019-07-31 13:04:54 -0700

    added .gitignore to base directory and a couple of settings changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+import hosts from "hosts";
+import addresses from "addresses";
+
 module.exports = {
     networks: {
         main: {
@@ -9,10 +12,10 @@ module.exports = {
             gasPrice: 10000000000
         },
         kovan: {
-            host: "ubuntu@ec2-18-218-112-231.us-east-2.compute.amazonaws.com",
+            host: hosts[0],
             port: 8545,
             network_id: 42,
-            from: '0x293b7c643e4c9a4362aac71f69862035819bc20e',
+            from: addresses[0],
             gas: 6000000,
             gasPrice: 1000000000
         }
