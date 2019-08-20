23ad65f5cb29361e649fdd62792d991d8281cf5b
Joseph Reed <chicobitcoinjoe@gmail.com>
2019-08-19 20:59:32 -0700

    moved factories to dedicated folder

---
+++ contracts/factories/FundraiserFactory.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+import "./external/CloneFactory.sol";
+import "./Fundraiser.sol";
+
+contract FundraiserFactory is CloneFactory {
+
+    function create () public view returns (Fundraiser fundraiser){
+
+    }
+
+}
